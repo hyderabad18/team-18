83594cfaadbc11a10426f7e270be6194d44c1acd
Manisha <manishabejugam9@gmail.com>
2018-07-14 19:14:47 +0530

    checkbox values inserted

---
+++ registration.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cfgteam18";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$checkbox1=$_POST['disabilities'];  
+$chk="";  
+foreach($checkbox1 as $chk1)  
+   {  
+      $chk .= $chk1.",";  
+   }  
+
+$sql = "INSERT INTO student(username,age,email,university,education,passedout,disabilities,phone,password,refercol)
+VALUES ('$_POST[student]','$_POST[age]','$_POST[studentemail]','$_POST[college]','$_POST[education]','$_POST[passout]','$chk','$_POST[phone]','$_POST[password]','$_POST[refercol]')";
+
+ 
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
