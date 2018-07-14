9be0fa9606df81543272aa58a39ae262cd93864b
Saipriya Peddi <37661361+SaipriyaPeddi@users.noreply.github.com>
2018-07-14 21:18:57 +0530

    Add files via upload

---
+++ cfg/company.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cfgteam18";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if($conn==null) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$comname=$_POST['comname'];
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$email=$_POST['email'];
+$address=$_POST['address'];
+$password=$_POST['password'];
+$refcol=$_POST['refcol'];
+
+$Createsql = "INSERT INTO company(comname, name, phone,email,address,password,refercom) VALUES ('$comname' , '$name' , '$phone', '$email', '$address', '$password','$refcol')";
+$res = mysqli_query($conn, $Createsql);
+if($res) {
+	echo "registered successfully";
+}
+else {
+    echo "Error: " . $Createsql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
