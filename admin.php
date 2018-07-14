87de89aa28fff52ec38a9a8b2a2f8f1140b2fe43
Manisha <manishabejugam9@gmail.com>
2018-07-15 03:09:07 +0530

    admin page

---
+++ admin.php
@@ -0,0 +1,6 @@
+
+<a href="adminstud.php">SHOW STUDENTS</a><br><br>
+<a href="admincomp.php">SHOW COMPANIES</a><br><br>
+<a href="adminjob.php">SHOW JOB PROFILES</a><br><br>
+<a href="admincol.php">SHOW COLLEGES</a><br><br>
+
