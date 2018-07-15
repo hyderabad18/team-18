332056ce03e005cb3c7215fec320c81b87cdb0f6
Manisha <manishabejugam9@gmail.com>
2018-07-15 07:32:39 +0530

    students redirection page

---
+++ studentsopenpage.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<h2>PARTICE TESTS</h2>
+
+<table>
+  <tr>
+    <th>TESTS</th>
+    <th>SCORE</th>
+    <th>TOTAL SCORE</th>
+    <th>ATTEMPT TEST</th>
+  </tr>
+  <tr>
+    <td>Test1</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+  <tr>
+    <td>Test2</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+  <tr>
+    <td>Test3</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+  <tr>
+    <td>Test4</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+  <tr>
+    <td>Test5</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+  <tr>
+    <td>Test6</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+  <tr>
+    <td>Test7</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+  <tr>
+    <td>Test8</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+  <tr>
+    <td>Test9</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+  <tr>
+    <td>Test10</td>
+    <td></td>
+    <td>100</td>
+    <td><button type="button">Attempt</button></td>
+  </tr>
+
+  
+</table>
+
+</body>
+</html>
